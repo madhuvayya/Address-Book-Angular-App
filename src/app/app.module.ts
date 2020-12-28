c906fda00437c5fc4664940cb22d96e90b298047
madhuvayya <madhu.vayya@gmail.com>
2020-12-29 00:17:30 +0530

    Added FormGroup and formControlNames on form fields to track the fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,11 +12,13 @@ import { AddressFormComponent } from './components/address-form/address-form.com
   declarations: [
     AppComponent,
     HeaderComponent,
-    AddressFormComponent
+    AddressFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/address-form/address-form.component.ts
+++ src/app/components/address-form/address-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-address-form',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address-form.component.scss']
 })
 export class AddressFormComponent implements OnInit {
+  contact: FormGroup;
+  errorText: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.contact = new FormGroup({
+      fullName: new FormControl(),
+      phoneNumber: new FormControl(),
+      address: new FormControl(),
+      city: new FormControl(),
+      state: new FormControl(),
+      zipcode: new FormControl()
+    })
   }
+
+  save(): void{
+    console.log(this.contact.value);
+  }
+
 
 }
