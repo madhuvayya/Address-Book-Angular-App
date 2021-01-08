b16b788eb89db4f1343e5ed7a32b52a626fbcae5
madhuvayya <madhu.vayya@gmail.com>
2021-01-08 20:53:27 +0530

    Added code for home page content heading

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  contactsList: Array<any>;
 
   constructor() { }
 
