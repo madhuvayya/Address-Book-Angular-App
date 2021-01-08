b2a0d51a5082c37e34209003eb811084b96a05d9
madhuvayya <madhu.vayya@gmail.com>
2021-01-08 16:10:14 +0530

    Added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AddressFormComponent } from './components/address-form/address-form.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AddressFormComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
